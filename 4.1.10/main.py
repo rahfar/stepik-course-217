b9a9ed1a54c4b34bf59968d0d1e37d02f8e49f58
rahfar <rahfar777@gmail.com>
2020-01-21 03:08:13 +0300

    4.1.10 - passed

---
+++ 4.1.10/main.py
@@ -0,0 +1,26 @@
+import math
+
+def foo(n):
+    # k = math.floor((-1+math.sqrt(1+8*n)/2))
+    k = 1
+    flg = True
+    ans = []
+    while flg:
+        if n - k < 0:
+            flg = False
+            ans[-1] += n
+            k -= 1
+        else:
+            ans.append(k)
+            n -= k
+            k += 1
+    print(k)
+    print(*ans, sep=' ')
+    return 0
+
+def main():
+    n = int(input())
+    foo(n)
+
+if __name__ == "__main__":
+    main()
---
+++ 4.1.10/test.py
@@ -0,0 +1,35 @@
+import unittest
+from unittest.mock import patch
+from main import main
+import io
+import sys
+
+class Test(unittest.TestCase):
+    def test_1(self):
+        capturedOutput = io.StringIO()
+        sys.stdout = capturedOutput
+        user_input = '''4
+'''.split('\n')
+        expected_output = '''2
+1 3
+'''
+        with patch('builtins.input', side_effect=user_input):
+            result = main()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(capturedOutput.getvalue(), expected_output)
+
+    def test_2(self):
+        capturedOutput = io.StringIO()
+        sys.stdout = capturedOutput
+        user_input = '''6
+'''.split('\n')
+        expected_output = '''3
+1 2 3
+'''
+        with patch('builtins.input', side_effect=user_input):
+            result = main()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(capturedOutput.getvalue(), expected_output)
+
+if __name__ == '__main__':
+    unittest.main()
